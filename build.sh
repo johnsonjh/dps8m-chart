0239d34793f1addcdb10fad83bb7ef5278f6b954
Jeffrey H. Johnson <trnsz@pobox.com>
2025-06-15 08:10:06 -0400

    Update build.sh
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@
 # shellcheck disable=SC2015
 command -v minify >/dev/null 2>&1 && {
   # minify from https://github.com/tdewolff/minify.git
-  minify source.html >index.html
-} || cp -f source.html index.html
+  printf '%s\n' "* minify source.html > index.html" 2> /dev/null || true
+  minify source.html > index.html
+} || {
+  printf '%s\n' "* cp -f source.html index.html" 2> /dev/null || true
+  cp -f source.html index.html
+}
