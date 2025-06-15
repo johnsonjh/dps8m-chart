025ad1724bfc207570e682081093b96017151033
Jeffrey H. Johnson <trnsz@pobox.com>
2025-06-15 07:47:51 -0400

    Add comments for build.sh
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
+# minify from https://github.com/tdewolff/minify.git
+# shellcheck disable=SC2015
 command -v minify >/dev/null 2>&1 && {
   minify source.html >index.html
 } || cp -f source.html index.html
